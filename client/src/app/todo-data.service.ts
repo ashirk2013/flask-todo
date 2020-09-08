d0f2e8993d9ff623063be3f288b858aa3413067c
Aaron Shirk <ashirk2013@gmail.com>
2020-09-07 21:01:25 -0400

    add some todo data logic

--- client/src/app/todo-data.service.ts
+++ client/src/app/todo-data.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Todo } from './todo';
+import { TagPlaceholder } from '@angular/compiler/src/i18n/i18n_ast';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoDataService {
+  lastId: number = 0;
+  todos: Todo[] = [];
 
   constructor() { }
+
+  // simulate POST /todos
+  addTodo(todo: Todo): TodoDataService {
+    if (!todo.id) {
+      todo.id = this.lastId++;
+    }
+    this.todos.push(todo);
+    return this;
+  }
+
+  // simulate DELETE /todos/:id
+  deleteTodoById(id: number): TodoDataService {
+    this.todos = this.todos.filter(todo => todo.id != id)
+    return this;
+  }
 }
